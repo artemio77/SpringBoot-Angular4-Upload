b646fd1e642ff3b24e00bd78a19fa0b3538d29ac
artemio77 <derevets.artem@gmail.com>
2018-08-20 12:34:14 +0300

    Initial commit

---
+++ SpringBootUploadFile/src/main/java/com/javasampleapproach/uploadfiles/SpringBootUploadFileApplication.java
@@ -0,0 +1,28 @@
+package com.javasampleapproach.uploadfiles;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.javasampleapproach.uploadfiles.storage.StorageService;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringBootUploadFileApplication implements CommandLineRunner {
+
+	@Resource
+	StorageService storageService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadFileApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
+}
